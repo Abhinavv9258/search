b9a62f609b621271e835f97d6de72ec3d8fb565f
Abhinav Verma <abhinavv218@gmail.com>
2023-10-03 23:56:47 +0530

    add cards and content

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,52 +1,55 @@
 import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
+import { Card, Button } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 import '../assets/css/Cards.css'
 
 const Cards = ({ papers }) => {
-    const [expandedIds, setExpandedIds] = React.useState([]);
+    const [expandedCitations, setExpandedCitations] = React.useState([]);
+    const [expandedAbstracts, setExpandedAbstracts] = React.useState([]);
+
+    const toggleVisibility = (paperId, type) => {
+        if (type === 'citation') {
+            if (expandedCitations.includes(paperId)) {
+                setExpandedCitations(expandedCitations.filter((id) => id !== paperId));
+            } else {
+                setExpandedCitations([...expandedCitations, paperId]);
+            }
+        } else if (type === 'abstract') {
+            if (expandedAbstracts.includes(paperId)) {
+                setExpandedAbstracts(expandedAbstracts.filter((id) => id !== paperId));
+            } else {
+                setExpandedAbstracts([...expandedAbstracts, paperId]);
+            }
+        }
+    };
+
+    const isCitationExpanded = (paperId) => expandedCitations.includes(paperId);
+    const isAbstractExpanded = (paperId) => expandedAbstracts.includes(paperId);
 
     let paperElements = null;
     let errorElements = null;
 
-    const toggleVisibility = (paperId) => {
-        // setExpandedPaperId(paperId);
-        if (expandedIds.includes(paperId)) {
-            // If already expanded, remove from the list
-            setExpandedIds(expandedIds.filter((id) => id !== paperId));
-        } else {
-            // If collapsed, add to the list
-            setExpandedIds([...expandedIds, paperId]);
-        }
-    };
-
-    // const isExpanded = (paperId) => {
-    //     return expandedPaperId === paperId;
-    // };
-
-    const isExpanded = (paperId) => expandedIds.includes(paperId);
-
-
-    const [expandedPaperId, setExpandedPaperId] = React.useState(null);
-
-
     try {
         paperElements = papers.map((paper) => {
+            const paperId = paper.paperId;
             const title = paper.title;
             const abstract = paper.abstract;
             const url = paper.url;
             const citations = paper.citationStyles.bibtex;
-            const paperId = paper.paperId;
+            const visibleCitations =
+                isCitationExpanded(paperId) || !citations
+                    ? citations
+                    : citations.length > 150
+                        ? `${citations.substring(0, 150)}...`
+                        : citations;
             const visibleAbstract =
-                isExpanded(paperId) || !abstract
-                    ? abstract // Display the full abstract if it's expanded or if it's null
+                isAbstractExpanded(paperId) || !abstract
+                    ? abstract
                     : abstract.length > 150
-                        ? `${abstract.substring(0, 150)}...` // Display the first 150 characters followed by "..."
-                        : abstract; // Display the entire abstract if it's shorter than 150 characters
-            console.log(visibleAbstract);
-
+                        ? `${abstract.substring(0, 150)}...`
+                        : abstract;
             return (
                 <div>
                     <div className='cards' key={paper.paperId}>
@@ -56,24 +59,41 @@ const Cards = ({ papers }) => {
                                 <Card.Text>{paperId}</Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
-                                <ListGroup.Item>{citations}</ListGroup.Item>
-                                {/* <ListGroup.Item>{abstract}</ListGroup.Item> */}
+                                <ListGroup.Item>
+                                    {citations && (
+                                        <>
+                                            {visibleCitations}
+                                            {citations.length > 150 && (
+                                                <div>
+                                                    <Button className='card-btn' onClick={() => toggleVisibility(paperId, 'citation')}>
+                                                        {isCitationExpanded(paperId)
+                                                            ? 'Show Less'
+                                                            : 'Show More'}
+                                                    </Button>
+                                                </div>
+                                            )}
+                                        </>
+                                    )}
+                                </ListGroup.Item>
                                 <ListGroup.Item>
                                     {abstract && (
                                         <>
                                             {visibleAbstract}
-                                            {abstract.length > 150 && !isExpanded(paperId) && (
-                                                <button onClick={() => toggleVisibility(paperId)}>
-                                                    {isExpanded(paperId) ? 'Show Less' : 'Show More'}
-                                                </button>
+                                            {abstract.length > 150 && (
+                                                <div>
+                                                    <Button className='card-btn' onClick={() => toggleVisibility(paperId, 'abstract')}>
+                                                        {isAbstractExpanded(paperId)
+                                                            ? 'Show Less'
+                                                            : 'Show More'}
+                                                    </Button>
+                                                </div>
                                             )}
                                         </>
                                     )}
-
                                 </ListGroup.Item>
                             </ListGroup>
                             <Card.Body>
-                                <Card.Link href={url}>Paper Link</Card.Link>
+                                <Card.Link  href={url}>Paper Link</Card.Link>
                             </Card.Body>
                         </Card>
                     </div>
@@ -91,6 +111,7 @@ const Cards = ({ papers }) => {
 
     return (
         <div>
+            {paperElements.length > 0 && <Card.Title style={{marginLeft:"5%"}}> Web Search </Card.Title>}
             {paperElements}
             {errorElements}
         </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,18 +2,30 @@ import React, { useState } from 'react';
 import Cards from './Cards';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../assets/css/Content.css'
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 const Content = () => {
     const [query, setQuery] = useState('');
     const [results, setResults] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [searchTagLocation, setSearchTagLocation] = useState('100vh');
 
     const handleInputChange = (event) => {
         setQuery(event.target.value);
     };
 
-    const handleSearch = async () => {
+    const handleSearch = (event) => {
+        // Check if the "Enter" key (key code 13) was pressed
+        if (event.key === 'Enter') {
+            performSearch(query);
+        }
+    };
+
+    const performSearch = async () => {
         setLoading(true);
         setError(null);
         try {
@@ -41,38 +53,65 @@ const Content = () => {
                 }
             };
             const results = await searchPublication(query);
-            if(!results){
-                toast.error("No results found, try changing search keywords!", {
+            if (!results) {
+                setSearchTagLocation('auto');
+                toast.info("No results found, try changing search keywords!", {
                     position: toast.POSITION.TOP_RIGHT,
                     autoClose: 3000,
                 })
             }
             setResults(results);
         } catch (error) {
+            setSearchTagLocation('100vh');
             setError('Error fetching results');
-            toast.error('Input is required!', {
+            toast.warning('Input is required!', {
                 position: toast.POSITION.TOP_RIGHT,
                 autoClose: 3000,
             });
         } finally {
+            setSearchTagLocation('auto');
             setLoading(false);
         }
     };
 
     return (
-        <div>
-            <input
+        <div className='search-container' style={{ height: `${searchTagLocation}`}}>
+            
+            {/* <input
                 type="text"
-                placeholder="Enter your research topic"
+                placeholder="Enter"
                 value={query}
                 onChange={handleInputChange}
+                prefix={<FaSearch />}
+
             />
-            <button onClick={handleSearch}>Search</button>
-            {loading ? (
-                <p>Loading...</p>
-            ) : error ? null : (
-                <Cards papers={results} />
-            )}
+            <button onClick={handleSearch}>Search</button> */
+            }
+
+            <div className='search'>
+                <div className='search-tag'>
+                    <input className="form-control search-board"
+                        type="search"
+                        value={query}
+                        onKeyDown={handleSearch}
+                        onChange={handleInputChange}
+                        placeholder="Search.."
+                        aria-label="Search" />
+                    <Button className='search-btn' onClick={performSearch} variant="outline-success">
+                        {/* <i className="bi bi-search"></i> */}
+                        {/* <p>Search</p> */}
+                        <FaCircleArrowRight className='search-icon' style={{ fontSize: '28px' }} />
+                    </Button>
+                </div>
+            </div>
+
+            <div className='search-result-container'>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : error ? null : (
+                    <Cards papers={results} />
+                )}
+            </div>
         </div>
     );
 };
